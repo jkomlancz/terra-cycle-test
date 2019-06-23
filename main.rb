a816a403d00a5615dff536435e9f364b31a5d70a
Komlancz Janos <jkomlancz@gmail.com>
2019-06-23 12:34:44 +0200

    i think it works with correct url

--- main.rb
+++ main.rb
@@ -8,44 +8,48 @@ require_relative "pullrequest"
 puts "Welcome!"
 
 # TODO: Be lehetne kerni milyen repon szeretnenk futtatni
-ghc = GitHubClient.new "jkomlancz", "terra-cycle-test-repo"
+$ghc = GitHubClient.new "jkomlancz", "terra-cycle-test-repo"
 
-$pulls = ghc.get_pulls
+$pulls = $ghc.get_pulls
 
 $all_modified_files = Array.new
-$result = Hash.new
 
 $pullrequests = $pulls.map { |pu| PullRequest.new(
     pu['url'],
     pu['number'],
     pu['commits_url'],
     # files
-    ghc.get_pull_files(pu['url']).map { |f| CommitFile.new(f['sha'], f['filename'], Helper.filter_modified_rows(f['patch']), f['blob_url']) }
+    $ghc.get_pull_files(pu['url']).map { |f| CommitFile.new(f['sha'], f['filename'], Helper.filter_modified_rows(f['patch']), "") }
     ) }
 
 def gather_modified_files
     $pullrequests.each do |pull|
-        pull.files.each do |file|
-            exists = false
-            if $all_modified_files.length == 0 then
-                $all_modified_files.push(file)
-            else
-                counter = 0
-                while counter < $all_modified_files.length do
-                    rf = $all_modified_files[counter]
-                    if rf.equals(file) && !(rf.urls.include? file.urls[0]) then
-                        rf.urls.push(file.urls[0])
-                        exists = true
-                    end
-                    counter += 1
-                end
-                if !exists
+        $ghc.get_call(pull.commits).each do |commit|
+            files = $ghc.get_call(commit['url'])['files'].map { |f| CommitFile.new(f['sha'], f['filename'], Helper.filter_modified_rows(f['patch']), commit['html_url']) }
+            files.each do |file|
+                exists = false
+                if $all_modified_files.length == 0 then
                     $all_modified_files.push(file)
+                else
+                    counter = 0
+                    while counter < $all_modified_files.length do
+                        rf = $all_modified_files[counter]
+                        if rf.equals(file) && !(rf.urls.include? file.urls[0]) then
+                            rf.urls.push(file.urls[0])
+                            exists = true
+                        end
+                        counter += 1
+                    end
+                    if !exists
+                        $all_modified_files.push(file)
+                    end
                 end
             end
         end
     end
 end
+
+# pull.files.each do |file|
 
 gather_modified_files()
 
